39d5b45f403627e8acda73525f4c45a2d4d83ffc
joellobo <joellobo@192.168.0.111>
2019-01-29 19:52:01 -0300

    refactoring

--- src/main/java/ytfeed/YTFeed.java
+++ src/main/java/ytfeed/YTFeed.java
@@ -5,26 +5,24 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.rometools.rome.feed.synd.SyndEntry;
 import com.rometools.rome.feed.synd.SyndFeed;
 import com.rometools.rome.feed.synd.SyndFeedImpl;
 import com.rometools.rome.io.SyndFeedInput;
 import com.rometools.rome.io.SyndFeedOutput;
 import com.rometools.rome.io.XmlReader;
 
-/**
- * It aggregates a list of RSS/Atom feeds (they can be of different types)
- * into a single feed of the specified type.
- * <p>
- * @author Alejandro Abdelnur
- *
- */
-public class FeedAggregator {
+public class YTFeed {
 
     public static void main(String[] args) {
-        boolean ok = false;
-        if (args.length>=2) {
+        
             try {
-                String outputType = args[0];
+            	List<String> urls = new ArrayList<String>();
+            	urls.add("https://www.youtube.com/feeds/videos.xml?channel_id=UCwRD2tsZ9iBz4f4LVBHgtVQ");
+            	urls.add("https://www.youtube.com/feeds/videos.xml?channel_id=UCKyTokYo0nK2OA-az-sDijA");
+            	
+            	// rss_0.9, rss_0.91U, rss_0.91N, rss_0.92, rss_0.93, rss_0.94, rss_1.0, rss_2.0 & atom_0.3
+                String outputType = "rss_2.0";
 
                 SyndFeed feed = new SyndFeedImpl();
                 feed.setFeedType(outputType);
@@ -34,11 +32,13 @@ public class FeedAggregator {
                 feed.setAuthor("anonymous");
                 feed.setLink("http://www.anonymous.com");
 
-                List entries = new ArrayList();
+                List<SyndEntry> entries = new ArrayList<SyndEntry>();
                 feed.setEntries(entries);
 
-                for (int i=1;i<args.length;i++) {
-                    URL inputUrl = new URL(args[i]);
+                
+                for (String url : urls) {
+	
+                    URL inputUrl = new URL(url);
 
                     SyndFeedInput input = new SyndFeedInput();
                     SyndFeed inFeed = input.build(new XmlReader(inputUrl));
@@ -50,22 +50,11 @@ public class FeedAggregator {
                 SyndFeedOutput output = new SyndFeedOutput();
                 output.output(feed,new PrintWriter(System.out));
 
-                ok = true;
             }
             catch (Exception ex) {
                 System.out.println("ERROR: "+ex.getMessage());
             }
-        }
 
-        if (!ok) {
-            System.out.println();
-            System.out.println("FeedAggregator aggregates different feeds into a single one.");
-            System.out.println("The first parameter must be the feed type for the aggregated feed.");
-            System.out.println(" [valid values are: rss_0.9, rss_0.91U, rss_0.91N, rss_0.92, rss_0.93, ]");
-            System.out.println(" [                  rss_0.94, rss_1.0, rss_2.0 & atom_0.3  ]");
-            System.out.println("The second to last parameters are the URLs of feeds to aggregate.");
-            System.out.println();
-        }
     }
 
 }
